aaa838813c688c6919b792a28d508a513ef464d3
ssatosays <ssatosays@gmail.com>
2022-12-04 13:38:20 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>init</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
