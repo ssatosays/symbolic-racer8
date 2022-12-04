9f24d0df28a5a3c2bb4041dcbebf6b90c05ebf31
ssatosays <ssatosays@gmail.com>
2022-12-04 17:15:37 +0900

    Add markers

--- src/App.vue
+++ src/App.vue
@@ -78,6 +78,66 @@ export default {
           latLng: latLng(35.681235294054765, 139.76712523292755),
           visible: true,
           code: ''
+        },
+        {
+          name: 'RVパーク 甲州市勝沼 ぶどうの丘', // 1th
+          latLng: latLng(35.673240140957205, 138.73579703736712),
+          visible: true,
+          code: 'xqZYod_atfY'
+        },
+        {
+          name: 'RVパーク ヘリテイジ美の山', // 2th
+          latLng: latLng(36.06632357666182, 139.11613247189374),
+          visible: true,
+          code: '_TlIq94TZi0'
+        },
+        {
+          name: '美の山公園', // 3th
+          latLng: latLng(36.053520817625746, 139.11299614042457),
+          visible: true,
+          code: 'fgqg2RPVwtw'
+        },
+        {
+          name: 'RVパーク 七里川', // 4th
+          latLng: latLng(35.2168524847308, 140.13591765572258),
+          visible: true,
+          code: 'W2aEWkKRwNk'
+        },
+        {
+          name: 'RVパーク 七里川', // 5th
+          latLng: latLng(35.217033599755794, 140.13485565649597),
+          visible: true,
+          code: 'rkwDRaLJsL4'
+        },
+        {
+          name: '長池親水公園', // 6th
+          latLng: latLng(35.42704072619624, 138.87118703895496),
+          visible: true,
+          code: 'O2j41-q6vtU'
+        },
+        {
+          name: '青根キャンプ場', // 7th
+          latLng: latLng(35.54843760181532, 139.13728809810797),
+          visible: true,
+          code: 'XQRVk-enarY'
+        },
+        {
+          name: '湯河原温泉神谷キャンプ場', // 8th
+          latLng: latLng(35.154880791619796, 139.07415324866656),
+          visible: true,
+          code: 'NvpQt3fmBHc'
+        },
+        {
+          name: 'RVパークsmart 山の駅 養老渓谷 喜楽里', // 9th
+          latLng: latLng(35.24134038765043, 140.17163616291464),
+          visible: true,
+          code: 'N0DatS4GyMI'
+        },
+        {
+          name: 'ネイチャーランドオム', // 10th
+          latLng: latLng(35.52447139267352, 139.03950733914553),
+          visible: true,
+          code: 'pCFyqzMeGaw'
         }
       ],
       youtubeUrl: ''
